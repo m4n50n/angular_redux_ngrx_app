64a662ce59cb6fe732db22a230b5b0dce1e4d17d
Jose Clemente Garcia Rodriguez <josegarciarodriguez89@hotmail.com>
2023-02-16 12:20:26 +0100

    Create reducer

--- src/app/contador/contador.reducer.ts
+++ src/app/contador/contador.reducer.ts
@@ -1,16 +1,28 @@
-import { Action } from "@ngrx/store";
+import { Action, createReducer, on } from "@ngrx/store";
 import { incrementar, decrementar } from './contador.actions';
 
 // Reducer recibirá las acciones
 // El reducer no debe trabajar con el "exterior"
 // Lo que el reducer haga debe hacerlo solo con la información que reciba
-export const contadorReducer = (state: number = 10, action: Action) => {
-    switch (action.type) {
-        case incrementar.type:
-            return state + 1; // Se recomienda hacer esto para retornar un nuevo estado, ya que si hago += 1 lo que retornaría sería una mutación del estado anterior
-        case decrementar.type:
-            return state - 1;
-        default:
-            return state;
-    }
-}
+// export const contadorReducer = (state: number = 10, action: Action) => {
+//     switch (action.type) {
+//         case incrementar.type:
+//             return state + 1; // Se recomienda hacer esto para retornar un nuevo estado, ya que si hago += 1 lo que retornaría sería una mutación del estado anterior
+//         case decrementar.type:
+//             return state - 1;
+//         default:
+//             return state;
+//     }
+// }
+
+
+/**
+ * Implementacion automática del reducer de NgRx según la documentación
+ */
+export const initialState = 20;
+
+export const contadorReducer = createReducer( // De esta forma ya no trabajamos con los switch que trabajaríamos con vanilla js
+    initialState,
+    on(incrementar, (state) => state + 1), // On permite seleccionar más rápidamente la acción que estoy disparando
+    on(decrementar, (state) => state - 1) // Automáticamente ya se recogerán las actions creadas con createAction en contador.actions.ts
+);
